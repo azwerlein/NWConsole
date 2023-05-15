39ad9e0f8f848714cb5c74ec346b0be8538a4fc2
azwerlein <azwerlein>
2023-05-15 14:02:09 -0500

    Run scaffolding

--- entity/Product.cs
+++
@@ -1,9 +0,0 @@
-public class Product
-{
-    public int ProductId { get; set; }
-    public string ProductName { get; set; }
-    public int SupplierId { get; set; }
-    public int CategoryId { get; set; }
-    public string QuantityPerUnit { get; set; }
-    public double UnitPrice { get; set; }
-}
---
+++ models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NWConsole.models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            Products = new HashSet<Product>();
+        }
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+        public string? Description { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
+    }
+}
